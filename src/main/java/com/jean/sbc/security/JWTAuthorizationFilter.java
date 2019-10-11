521243286c3fc7bd5ea222664ccb5f656873b0dc
Jean Briesemeister <jeanbr86@gmail.com>
2019-10-11 06:16:27 -0300

    Added authorization for users

---
+++ src/main/java/com/jean/sbc/security/JWTAuthorizationFilter.java
@@ -0,0 +1,55 @@
+package com.jean.sbc.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+	private JWTUtil jwtUtil;
+
+	private UserDetailsService userDetailsService;
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil,
+			UserDetailsService userDetailsService) {
+		super(authenticationManager);
+		this.jwtUtil = jwtUtil;
+		this.userDetailsService = userDetailsService;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		super.doFilterInternal(request, response, chain);
+
+		String authorization = request.getHeader("Authorization");
+		if (authorization != null && authorization.startsWith("Bearer ")) {
+			UsernamePasswordAuthenticationToken authToken = this.getAuthentication(authorization.substring(7));
+			if (authToken != null) {
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(String authorization) {
+		if (jwtUtil.isValidToken(authorization)) {
+			String userName = jwtUtil.getUserName(authorization);
+			UserDetails user = userDetailsService.loadUserByUsername(userName);
+			return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+		}
+		return null;
+	}
+
+}
--- src/main/java/com/jean/sbc/security/JWTUtil.java
+++ src/main/java/com/jean/sbc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -20,6 +21,37 @@ public class JWTUtil {
 	public String generateToken(String userName) {
 		return Jwts.builder().setSubject(userName).setExpiration(new Date(System.currentTimeMillis() + this.expiration))
 				.signWith(SignatureAlgorithm.HS512, this.secret.getBytes()).compact();
+	}
+
+	public boolean isValidToken(String authorization) {
+		Claims claims = this.getClaims(authorization);
+		if (claims != null) {
+			String userName = claims.getSubject();
+			Date expiration = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+
+			if (userName != null && expiration != null && now.before(expiration)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private Claims getClaims(String authorization) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(authorization).getBody();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getUserName(String authorization) {
+		Claims claims = this.getClaims(authorization);
+		if (claims != null) {
+			return claims.getSubject();
+		}
+		
+		return null;
 	}
 
 }
